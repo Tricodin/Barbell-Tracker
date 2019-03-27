520557c35347edd32394edf83f3dfb6cbf892126
abporter <abporter@mun.ca>
2019-03-27 14:58:03 -0230

    Can now export to .csv

--- app/src/main/java/com/example/opencvproject/Lift_History.java
+++ app/src/main/java/com/example/opencvproject/Lift_History.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,10 +18,24 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Context;
+
+
+
+
 
 import com.example.opencvproject.detector.DatabaseHelper;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Lift_History extends AppCompatActivity {
+
+
 
     DatabaseHelper mDatabaseHelper = new DatabaseHelper(this);
        private TableLayout table;
@@ -35,25 +51,7 @@ public class Lift_History extends AppCompatActivity {
 
     }
 
-    public void showData(View v) {
-        Cursor res = mDatabaseHelper.getData();
-        if (res.getCount() == 0) {
-            toastMessage("Database Empty");
-            return;
-        } else {
-            StringBuffer buffer = new StringBuffer();
-            while (res.moveToNext())
-            {
-                buffer.append("Index :"+ res.getString(0) + "\n");
-                buffer.append("Date :"+ res.getString(1) + "\n");
-                buffer.append("Deviation :"+ res.getString(2) + "\n");
-                buffer.append("Uri :"+ res.getString(3) + "\n\n");
 
-
-            }
-            showMessage("Data", buffer.toString());
-        }
-    }
 
     public void showMessage(String title, String Message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -110,6 +108,67 @@ public class Lift_History extends AppCompatActivity {
             tr.addView(b1);
 
             table.addView(tr);
+        }
+
+    }
+
+    public void saveCSV(View view)
+    {
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        final String appName = getString(R.string.app_name);
+
+
+
+
+        Cursor res = mDatabaseHelper.getData();
+        if (res.getCount() == 0) {
+            toastMessage("Database Empty");
+            return;
+        } else {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("Index, Date, Deviation \n");
+            while (res.moveToNext())
+            {
+                buffer.append(res.getString(0) + ",");
+                buffer.append(res.getString(1) + ",");
+                buffer.append(res.getString(2) + "\n");
+
+
+
+            }
+            //showMessage("Data", buffer.toString());
+            final File path =
+                    Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_DOCUMENTS );
+
+            if(!path.exists())
+            {
+                path.mkdirs();
+            }
+            String filename = "Lift_History" + date + ".csv";
+            final File file  = new File(path, filename);
+
+
+            try
+            {
+                file.createNewFile();
+                FileOutputStream fOut = new FileOutputStream(file);
+                OutputStreamWriter myOutWrite = new OutputStreamWriter(fOut);
+                myOutWrite.append(buffer);
+
+                myOutWrite.close();
+
+                fOut.flush();
+                fOut.close();
+
+                toastMessage("Written To Documents Folder");
+            }
+
+            catch(IOException e)
+            {
+                Log.e("Exception", "WriteFailed");
+            }
+
         }
 
     }
